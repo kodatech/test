bf0dc7649946f94e07b36a89e1a65435f9a6bf94
dave.wood@kodait.com <dave.wood@kodait.com>
2016-11-15 11:59:57 +1300

    despues de test

--- js/index.js
+++ js/index.js
@@ -3,7 +3,33 @@ var app;
 app = {
     // Application Constructor
     initialize: function () {
-        this.bindEvents();
+        //this.bindEvents();
+        alert("bindEvents")
+        //alert("hi")
+        // Add to index.js or the first page that loads with your app.
+        // For Intel XDK and please add this to your app.js.
+
+        //document.addEventListener('deviceready', this.onDeviceReady, false);
+
+        document.addEventListener('deviceready', function () {
+          
+
+          // Enable to debug issues.
+          window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+          
+          var notificationOpenedCallback = function(jsonData) {
+            console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+          };
+
+          window.plugins.OneSignal
+            .startInit("933b8ea9-4bd9-4acf-84e1-7d7fcdb4c784", "144953997036")
+            .handleNotificationOpened(notificationOpenedCallback)
+            .endInit();
+          
+          // Sync hashed email if you have a login system or collect it.
+          //   Will be used to reach the user at the most optimal time of day.
+          // window.plugins.OneSignal.syncHashedEmail(userEmail);
+        }, false);
     },
 
     bindEvents: function () {
@@ -15,10 +41,10 @@ app = {
         //document.addEventListener('deviceready', this.onDeviceReady, false);
 
         document.addEventListener('deviceready', function () {
-          alert("deviceready")
+          
 
           // Enable to debug issues.
-          // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+          window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
           
           var notificationOpenedCallback = function(jsonData) {
             console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
