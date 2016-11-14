d7466d80a2b61ddf6230878a982a12fb2d739144
dave.wood@kodait.com <dave.wood@kodait.com>
2016-11-15 09:25:21 +1300

    despues de test

--- js/index.js
+++ js/index.js
@@ -1,17 +1,33 @@
-document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
+app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
 
-  window.plugins.OneSignal
-    .startInit("933b8ea9-4bd9-4acf-84e1-7d7fcdb4c784", "144953997036")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-  
-  // Sync hashed email if you have a login system or collect it.
-  //   Will be used to reach the user at the most optimal time of day.
-  // window.plugins.OneSignal.syncHashedEmail(userEmail);
-}, false);
+    bindEvents: function () {
+
+        //alert("hi")
+        // Add to index.js or the first page that loads with your app.
+        // For Intel XDK and please add this to your app.js.
+
+        document.addEventListener('deviceready', function () {
+
+          // Enable to debug issues.
+          // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+          
+          var notificationOpenedCallback = function(jsonData) {
+            console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+          };
+
+          window.plugins.OneSignal
+            .startInit("933b8ea9-4bd9-4acf-84e1-7d7fcdb4c784", "144953997036")
+            .handleNotificationOpened(notificationOpenedCallback)
+            .endInit();
+          
+          // Sync hashed email if you have a login system or collect it.
+          //   Will be used to reach the user at the most optimal time of day.
+          // window.plugins.OneSignal.syncHashedEmail(userEmail);
+        }, false);
+    }
+
+};
